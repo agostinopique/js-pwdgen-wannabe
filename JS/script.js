11efef86cf242f9b37b14631248abe98522bb83b
agostinopique <ago.pique@yahoo.it>
2022-04-05 14:39:58 +0200

    First commit;

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+// console.log('Hello world');
+
+const firstName = prompt('What is your name?');
+
+const secondName = prompt('What is your surname?');
+
+const favoriteColor = prompt('What is your favorite color?')
+
+const lastNumber = 22;
+
+// console.log(firstName, secondName, favoriteColor);
+
+document.getElementById('pwd-generated').innerHTML += firstName + secondName + favoriteColor + lastNumber;
